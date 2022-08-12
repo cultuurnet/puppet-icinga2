942e6d03e417e147f950ed98a7a8cf46295837d1
Kristof Willaert <kristof@publiq.be>
2022-08-12 10:42:44 +0200

    Blindly add Ubuntu 18.04 support

--- manifests/params.pp
+++ manifests/params.pp
@@ -113,7 +113,7 @@ class icinga2::params {
           $server_plugin_package_install_options = '--no-install-recommends'
         }
         #...but 14.04 does:
-        '14.04','16.04': {
+        '14.04','16.04','18.04': {
           $icinga2_server_package = 'icinga2'
           $icinga2_server_plugin_packages = [ 'nagios-plugins', 'nagios-plugins-basic', 'nagios-plugins-common', 'nagios-plugins-standard', 'nagios-snmp-plugins', 'nagios-plugins-extra', 'nagios-plugins-contrib', 'nagios-nrpe-plugin']
           $icinga2_server_mail_package = 'mailutils'
@@ -272,7 +272,7 @@ class icinga2::params {
         '12.04': {
           $icinga2_server_service_name = 'icinga2'
         }
-        '14.04','16.04': {
+        '14.04','16.04','18.04': {
           $icinga2_server_service_name = 'icinga2'
         }
         #Fail if we're on any other Ubuntu release:
@@ -387,7 +387,7 @@ class icinga2::params {
           $client_plugin_package_install_options = '--no-install-recommends'
         }
         #...but 14.04 does:
-        '14.04','16.04': {
+        '14.04','16.04','18.04': {
           $icinga2_client_packages = ['nagios-nrpe-server', 'nagios-plugins', 'nagios-plugins-basic', 'nagios-plugins-common', 'nagios-plugins-standard', 'nagios-snmp-plugins', 'nagios-plugins-extra', 'nagios-plugins-contrib', 'nagios-nrpe-plugin']
           #Specify '--no-install-recommends' so we don't inadvertently get Nagios 3 installed; it comes as a recommended package with most of the plugin packages:
           $client_plugin_package_install_options = '--no-install-recommends'
