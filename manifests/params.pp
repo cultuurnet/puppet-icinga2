6815f51dd5c8bf1227d1bec2b2705343a92ac39d
Kristof Willaert <kristof@publiq.be>
2023-05-03 14:31:25 +0200

    Blindly add Ubuntu 20.04 support

--- manifests/params.pp
+++ manifests/params.pp
@@ -120,7 +120,7 @@ class icinga2::params {
           #Specify '--no-install-recommends' so we don't inadvertently get Nagios 3 installed; it comes as a recommended package with most of the plugin packages:
           $server_plugin_package_install_options = '--no-install-recommends'
         }
-        '18.04': {
+        '18.04','20.04': {
           $icinga2_server_package = 'icinga2'
           $icinga2_server_plugin_packages = [ 'nagios-plugins', 'nagios-plugins-basic', 'nagios-plugins-common', 'nagios-plugins-standard', 'nagios-snmp-plugins', 'nagios-plugins-contrib', 'nagios-nrpe-plugin']
           $icinga2_server_mail_package = 'mailutils'
@@ -279,7 +279,7 @@ class icinga2::params {
         '12.04': {
           $icinga2_server_service_name = 'icinga2'
         }
-        '14.04','16.04','18.04': {
+        '14.04','16.04','18.04','20.04': {
           $icinga2_server_service_name = 'icinga2'
         }
         #Fail if we're on any other Ubuntu release:
