caab72a3835c7e8bc11c568156b33ee2ddf683a5
Kristof Willaert <kristof@publiq.be>
2023-05-03 13:56:55 +0200

    Blindly add Ubuntu 20.04 support

--- manifests/params.pp
+++ manifests/params.pp
@@ -399,7 +399,7 @@ class icinga2::params {
           #Specify '--no-install-recommends' so we don't inadvertently get Nagios 3 installed; it comes as a recommended package with most of the plugin packages:
           $client_plugin_package_install_options = '--no-install-recommends'
         }
-        '18.04': {
+        '18.04','20.04': {
           $icinga2_client_packages = ['nagios-nrpe-server', 'nagios-plugins', 'nagios-plugins-basic', 'nagios-plugins-common', 'nagios-plugins-standard', 'nagios-snmp-plugins', 'nagios-plugins-contrib', 'nagios-nrpe-plugin']
           #Specify '--no-install-recommends' so we don't inadvertently get Nagios 3 installed; it comes as a recommended package with most of the plugin packages:
           $client_plugin_package_install_options = '--no-install-recommends'
